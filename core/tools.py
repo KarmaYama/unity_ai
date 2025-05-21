f7d5f78e526deeae72a8f6c1b55c5615b443fa0e
KarmaYama <alex.matarirano@gmail.com>
2025-05-21 18:13:54 +0200

    remove langgraph tool executor

--- core/tools.py
+++ core/tools.py
@@ -1,17 +1,13 @@
 from langchain_community.tools import DuckDuckGoSearchRun
 from langchain.agents import Tool
-from langgraph import ToolExecutor  
 
-def setup_tools() -> ToolExecutor:
-    """
-    Initialize and return a ToolExecutor with the available tools.
-    """
+def setup_tools():
     search = DuckDuckGoSearchRun()
     tools = [
         Tool(
             name="DuckDuckGo Search",
             func=search.run,
-            description="Useful for searching the web for information."
+            description="Use this tool to search the web for current information."
         )
     ]
-    return ToolExecutor(tools)
+    return tools
